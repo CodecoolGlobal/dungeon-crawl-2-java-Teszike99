feb515a6a651525dccd040dcc5bbe3331016c4c7
kistegez <tegeza@gmail.com>
2022-01-26 15:37:53 +0100

    Fix - game_sate_id instead of map_id and map table

--- sample_data/schema_ddl.sql
+++ sample_data/schema_ddl.sql
@@ -2,7 +2,6 @@ DROP TABLE IF EXISTS public.game_state;
 CREATE TABLE public.game_state (
     id serial NOT NULL PRIMARY KEY,
     current_map text NOT NULL,
-    map_id integer NOT NULL,
     saved_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
     player_id integer NOT NULL
 );
@@ -10,7 +9,7 @@ CREATE TABLE public.game_state (
 DROP TABLE IF EXISTS public.player;
 CREATE TABLE public.player (
     id serial NOT NULL PRIMARY KEY,
-    player_name text UNIQUE NOT NULL,
+    player_name text NOT NULL,
     hp integer NOT NULL,
     strength integer NOT NULL,
     x integer NOT NULL,
@@ -20,7 +19,7 @@ CREATE TABLE public.player (
 DROP TABLE IF EXISTS public.enemy;
 CREATE TABLE public.enemy (
     id serial NOT NULL PRIMARY KEY,
-    map_id integer NOT NULL,
+    game_state_id integer NOT NULL,
     enemy_name text NOT NULL,
     strength integer NOT NULL,
     hp integer NOT NULL,
@@ -31,26 +30,20 @@ CREATE TABLE public.enemy (
 DROP TABLE IF EXISTS public.item;
 CREATE TABLE public.item (
     id serial NOT NULL PRIMARY KEY,
-    map_id integer NOT NULL,
+    game_state_id integer NOT NULL,
     item_name text NOT NULL,
     x integer NOT NULL,
     y integer NOT NULL
 );
 
 DROP TABLE IF EXISTS public.map;
-CREATE TABLE public.map (
-    id serial NOT NULL PRIMARY KEY,
-    map_name char varying(10)
-);
 
 ALTER TABLE ONLY public.game_state
     ADD CONSTRAINT fk_player_id FOREIGN KEY (player_id) REFERENCES public.player(id);
 
-ALTER TABLE ONLY public.game_state
-    ADD CONSTRAINT fk_map_id FOREIGN KEY (map_id) REFERENCES public.map(id);
 
 ALTER TABLE ONLY public.enemy
-    ADD CONSTRAINT fk_map_id FOREIGN KEY (map_id) REFERENCES public.map(id);
+    ADD CONSTRAINT fk_game_state_id FOREIGN KEY (game_state_id) REFERENCES public.game_state(id);
 
 ALTER TABLE ONLY public.item
-    ADD CONSTRAINT fk_map_id FOREIGN KEY (map_id) REFERENCES public.map(id);
+    ADD CONSTRAINT fk_game_state_id FOREIGN KEY (game_state_id) REFERENCES public.game_state(id);
