68104452ecc987c703239d9afdae4940d4dcc579
kistegez <tegeza@gmail.com>
2022-01-30 21:40:19 +0100

    Feature - UniTest for Items

--- src/test/java/com/codecool/dungeoncrawl/logic/ItemTest.java
+++ src/test/java/com/codecool/dungeoncrawl/logic/ItemTest.java
@@ -1,11 +1,17 @@
 package com.codecool.dungeoncrawl.logic;
 
+import com.codecool.dungeoncrawl.logic.Items.Key;
+import com.codecool.dungeoncrawl.logic.Items.OpenedDoor;
 import com.codecool.dungeoncrawl.logic.Items.Potion;
 import com.codecool.dungeoncrawl.logic.Items.Sword;
+import com.codecool.dungeoncrawl.logic.actors.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedList;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class ItemTest {
     GameMap gameMap = new GameMap(3, 3, CellType.FLOOR);
@@ -23,5 +29,44 @@ public class ItemTest {
         Cell PotionCell = new Cell(gameMap, 1,1, CellType.FLOOR);
         Potion potion = new Potion(PotionCell);
         assertEquals(1, potion.getX());
+    }
+
+
+    @Test
+    void pickUpItem_playerCanPickUpItem(){
+        Player player = new Player(gameMap.getCell(2,2));
+        Sword sword = new Sword(gameMap.getCell(2,2));
+        LinkedList<String> playerInventory = new LinkedList<String>();
+        playerInventory.add("uzi");
+
+        player.pickUpItem();
+
+        assertEquals(playerInventory, player.getPlayerInventory());
+        assertNull(gameMap.getCell(2,2).getItem());
+    }
+
+    @Test
+    void openDoor_givesBackItsName(){
+        OpenedDoor door = new OpenedDoor(gameMap.getCell(2,2));
+
+        assertEquals("openDoor", door.getTileName());
+    }
+
+    @Test
+    void keyName_askingForKeyGivesItsName(){
+        Key key = new Key(gameMap.getCell(2,2));
+
+        assertEquals(gameMap.getCell(2,2), key.getCell());
+        assertEquals(2, key.getX());
+        assertEquals(2, key.getY());
+        assertEquals("key", key.getTileName());
+    }
+
+    @Test
+    void potionName_askingForPotionName(){
+        Potion potion = new Potion(gameMap.getCell(2,2));
+
+
+        assertEquals("cola", potion.getTileName());
     }
 }
